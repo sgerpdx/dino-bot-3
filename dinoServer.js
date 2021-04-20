c100f0298c7356f7bcc688153ca443a4a200923f
SamuelHGerber <henrylightfoot@gmail.com>
2021-04-20 16:57:48 -0700

    additional setup

---
+++ dinoServer.js
@@ -0,0 +1,18 @@
+// const { App } = require('@slack/bolt');
+
+// const app = new App({
+//   token: process.env.SLACK_BOT_TOKEN,
+//   signingSecret: process.env.SLACK_SIGNING_SECRET,
+// });
+
+// async () => {
+//   await app.start(process.env.PORT || 3000);
+//   console.log('Bolt app running');
+// };
+
+// app.event('app_home_opened', ({ event, say }) => {
+//   console.log(event);
+//   say(`Hello, <@${event.user}>!`);
+// });
+
+// app.start(process.env.port || 3000).then(() => console.log('Bolt app running'));
